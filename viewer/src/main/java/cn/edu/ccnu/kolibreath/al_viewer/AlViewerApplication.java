929471e422148c21b51fdf100f744bda8b3bd61a
kolibreath <734780178@qq.com>
2020-01-18 21:26:41 +0800

    [ADD] creating an viewer for bat algorithm

---
+++ viewer/src/main/java/cn/edu/ccnu/kolibreath/al_viewer/AlViewerApplication.java
@@ -0,0 +1,18 @@
+package cn.edu.ccnu.kolibreath.al_viewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class AlViewerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlViewerApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
---
+++ viewer/src/test/java/cn/edu/ccnu/kolibreath/al_viewer/AlViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.ccnu.kolibreath.al_viewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlViewerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
